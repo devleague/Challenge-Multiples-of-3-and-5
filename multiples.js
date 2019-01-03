0dca01ad443ff60279c6d7e668ef64cf6b76667e
Tyler <alexithemia@gmail.com>
2019-01-03 09:44:21 -1000

    revert to original

--- multiples.js
+++ multiples.js
@@ -5,17 +5,10 @@
  */
 exports.sumOfAMultiple = function (n) {
   var sum = 0;
-  if (n >= 0) {
-    for (let i = 1; i < n; i++) {
-      if (i % 3 === 0 || i % 5 === 0) {
-        sum += i;
-      }
-    }
-  } else {
-    for (let i = -1; i > n; i--) {
-      if (i % 3 === 0 || i % 5 === 0) {
-        sum += i;
-      }
+
+  for (let i = 1; i < n; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
     }
   }
   return sum;
