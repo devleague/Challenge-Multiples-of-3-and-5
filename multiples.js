565b360c0f3ffd709fdba5bd10c6740de1187db9
Tyler <alexithemia@gmail.com>
2019-01-03 09:38:48 -1000

    changes for negatives

--- multiples.js
+++ multiples.js
@@ -5,10 +5,17 @@
  */
 exports.sumOfAMultiple = function (n) {
   var sum = 0;
-
-  for (let i = 1; i < n; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
+  if (n >= 0) {
+    for (let i = 1; i < n; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+  } else {
+    for (let i = -1; i > n; i--) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
     }
   }
   return sum;
